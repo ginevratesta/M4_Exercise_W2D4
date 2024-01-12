cc17df720064858b9a8a8f53deee5bf184d2bab7
Ginevra Testa <ginevra99testaa@gmail.com>
2024-01-12 15:55:42 +0100

    style-change of plan

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ fetch("https://striveschool-api.herokuapp.com/books")
       const price = book.price;
       const category = book.category;
       marketPlace.innerHTML += `
-       <div class="col-12 col-sm-12 col-md-6 col-lg-3">
+       <div class="col-12 col-sm-12 col-md-3 col-lg-2 pb-4">
         <div class="card">
-  <img src="${cover}" class="card-img-top" alt="...">
+  <img src="${cover}" class="card-img-top w-100 h-100" alt="Book cover">
   <div class="card-body">
-    <h5 class="card-title">${title}</h5>
-    <p class="card-text">${category}.</p>
-    <a href="#" class="btn btn-primary">${price}</a>
+    <h5 class="card-title text-truncate">${title}</h5>
+    <p class="card-text">Genre: ${category}.</p>
+    <p class="card-text">Price: ${price}.</p>
   </div>
 </div>
 </div>`;
