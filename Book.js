22a604d5532d85ab81c7c78ac5ed48a9dc993f5e
Ginevra Testa <ginevra99testaa@gmail.com>
2024-01-13 18:21:02 +0100

    addBookToCart in progress

--- Book.js
+++ Book.js
@@ -1,22 +1,35 @@
 const getBooks = async () => {
-    try {
-        const res = await fetch("https://striveschool-api.herokuapp.com/books")
-        const data = await res.json()
-        return data;
-    } catch (error){
-        console.error("Error:" + error);
-        alert(error)
-    }
-    
-}
+  try {
+    const res = await fetch("https://striveschool-api.herokuapp.com/books");
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Error:" + error);
+    // alert(error)
+  }
+};
+
+const getBook = async (asin) => {
+  try {
+    const res = await fetch(
+      "https://striveschool-api.herokuapp.com/books/" + asin
+    );
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Error:" + error);
+    // alert(error)
+  }
+};
 
 const formatBookHtml = (book) => {
-    const title = book.title;
-    const cover = book.img;
-    const price = book.price;
-    const category = book.category;
-    const formattedHtml = `
-      <div class="book-card col-12 col-sm-12 col-md-3 col-lg-2 pb-4">
+  const title = book.title;
+  const cover = book.img;
+  const price = book.price;
+  const category = book.category;
+  const asin = book.asin;
+  const formattedHtml = `
+      <div id="${asin}" class="book-card col-12 col-sm-12 col-md-3 col-lg-2 pb-4">
         <div class="card">
           <img src="${cover}" class="card-img-top w-100 h-100" alt="Book cover">
           <div class="card-body">
@@ -30,15 +43,49 @@ const formatBookHtml = (book) => {
           </div>
         </div>
       </div>`;
-      return formattedHtml;
-  }
+  return formattedHtml;
+};
 
-const displayBooks = (books,marketPlace) => {
-    books.forEach((book) => {
-        //Displaying the book cards on the web page
-        const formattedHtml = formatBookHtml(book)
-        marketPlace.innerHTML += formattedHtml;
-      });
-}
+const addSkipActionButton = () => {
+  const skipButtons = document.querySelectorAll(".skip");
+  const cards = document.querySelectorAll(".book-card");
+  skipButtons.forEach((skip, index) => {
+    skip.addEventListener("click", (ev) => {
+      if (ev.target === skip) {
+        cards[index].style.display = "none";
+      }
+    });
+  });
+};
 
-export {getBooks,formatBookHtml,displayBooks}
+const addBookToCart = (books) => {
+  books.forEach((book) => {
+    const bookHtmlElement = document.getElementById(book.asin);
+    const cartSelections = document.querySelector(".dropdown-menu");
+    const cartButton = bookHtmlElement.querySelector(".cartButtons");
+    cartButton.addEventListener("click", async (ev) => {
+      const cardElement = ev.target.parentElement.parentElement.parentElement;
+      const bookData = await getBook(cardElement.id);
+      if (ev.target === cartButton) {
+        cartSelections.innerHTML += `<li class="d-flex align-items-center justify-content-between m-2">
+            <div class="img-cart-holder me-2">
+            <img class="w-100 h-100" src="${bookData.img}" alt="book cover"/>
+            </div>
+            <span class="text-truncate">${bookData.title}</span>
+            </li>`;
+            cartButton.style.color = "red";
+      }
+    });
+  });
+};
+
+const displayBooks = (books, marketPlace) => {
+  books.forEach((book) => {
+    const formattedHtml = formatBookHtml(book);
+    marketPlace.innerHTML += formattedHtml;
+  });
+  addSkipActionButton();
+  addBookToCart(books);
+};
+
+export { getBooks, formatBookHtml, displayBooks };
--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-import {getBooks, displayBooks} from "./Book.js";
+import {getBooks,displayBooks} from "./Book.js";
 
-
-// 1. prendo il dato
+// Taking the parameter with await because it is an asyncronous function
 const books = await getBooks()
 
-// 2. inserisco il dato nell'html
 const marketPlace = document.querySelector("#marketPlace");
 marketPlace.classList.add("row", "py-4", "ps-3", "w-100");
-const skipButtons = document.querySelectorAll(".skip");
-const cards = document.querySelectorAll(".book-card");
-
 displayBooks(books,marketPlace)
+
 
